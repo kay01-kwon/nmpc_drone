f77a5ba28ca8df42de4fb6c697ac5aadbe863103
kay <kay1216@yonsei.ac.kr>
2024-06-21 21:13:48 +0900

    quad model on going

--- nmpc_quad/nodes/model/quad_model.py
+++ nmpc_quad/nodes/model/quad_model.py
@@ -15,11 +15,23 @@ class QuadModel:
         self.C_lift = C_lift
         self.C_moment = C_moment
 
-        # Casadi: Set position, linear velocity
-        # quaternion and angular velocity
-        self.p = cs.MX.sym('p',3)
-        self.v = cs.MX.sym('v',3)
-        self.q = cs.MX.sym('q',4)
-        self.w = cs.MX.sym('w',3)
+        # Casadi: Assign state
+        self.p = cs.MX.sym('p',3)   # position
+        self.v = cs.MX.sym('v',3)   # velocity
+        self.q = cs.MX.sym('q',4)   # quaternion
+        self.w = cs.MX.sym('w',3)   # angular velocity
+        self.x = cs.vertcat(self.p, self.v, self.q, self.w)     #state
+        self.x_dim = 13
+
+        # Casadi: Assign four rotors' thrust control input
+        self.u1 = cs.MX.sym('u1')
+        self.u2 = cs.MX.sym('u2')
+        self.u3 = cs.MX.sym('u3')
+        self.u4 = cs.MX.sym('u4')
+        self.u = cs.vertcat(self.u1, self.u2, self.u3, self.u4)
+        self.u_dim = 4
         
+
+
+
 
