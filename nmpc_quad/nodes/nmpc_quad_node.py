9aac6f4e4d84ff0db1442328f1e65dc6c4b74985
kay <kay1216@yonsei.ac.kr>
2024-06-25 20:55:38 +0900

    nmpc node modified

--- nmpc_quad/nodes/nmpc_quad_node.py
+++ nmpc_quad/nodes/nmpc_quad_node.py
@@ -14,8 +14,7 @@ class nmpc_quad_node:
         self.state = np.zeros((13,))
         self.state[6] = 1.0
 
-        self.y_ref = np.zeros((17,))
-        self.y_ref_N = np.zeros((17,))
+        self.ref = self.state
 
         self.u_msg = Actuators()
 
@@ -32,23 +31,28 @@ class nmpc_quad_node:
 
     def state_callback(self, msg):
 
-        # Get state
+        # Get current position
         self.state[0] = msg.pose.pose.position.x
         self.state[1] = msg.pose.pose.position.y
         self.state[2] = msg.pose.pose.position.z
 
+        # Get current quaternion
         self.state[3] = msg.pose.pose.orientation.quaternion.x
         self.state[4] = msg.pose.pose.orientation.quaternion.y
         self.state[5] = msg.pose.pose.orientation.quaternion.z
         self.state[6] = msg.pose.pose.orientation.quaternion.w
 
+        # Get current linear velocity
         self.state[7] = msg.pose.pose.twist.twist.linear.x
         self.state[8] = msg.pose.pose.twist.twist.linear.y
         self.state[9] = msg.pose.pose.twist.twist.linear.z
 
+        # Get current angular velocity
         self.state[10] = msg.pose.pose.twist.twist.angular.x
         self.state[11] = msg.pose.pose.twist.twist.angular.y
         self.state[12] = msg.pose.pose.twist.twist.angular.z
+
+
 
 
 
