11c179c0e64076396d5258f63ba5499c8933386a
kay <kay1216@yonsei.ac.kr>
2024-06-25 20:30:00 +0900

    nmpc node

--- nmpc_quad/nodes/nmpc_quad_node.py
+++ nmpc_quad/nodes/nmpc_quad_node.py
@@ -1,10 +1,30 @@
-import quad_nmpc_pkg
-from acados_template import AcadosOcpSolver
+import numpy as np
+
+from nmpc_pkg import ocp_solver
 import rospy
+from nav_msgs.msg import Odometry
 from mav_msgs.msg import Actuators
 from std_srvs.srv import Empty
 
 class nmpc_quad_node:
     def __init__(self):
-        ocp_obj = quad_nmpc_pkg.ocp_sovler.OcpSovler()
-        self.ocp = ocp_obj.ocp_sovler.get_ocp_sovler()
+        # Create ocp solver object
+        self.ocp_solver_obj = ocp_solver.OcpSolver()
+
+        self.state = np.zeros((13,))
+
+        self.y_ref = np.zeros((17,))
+        self.y_ref_N = np.zeros((17,))
+
+        self.u_msg = Actuators()
+
+    def ros_setup(self):
+
+        # Input publisher to hummingbird
+        self.input_pub = rospy.Publisher('/hummingbird/command/motor_speed',
+                                         Actuators,
+                                         queue_size=1)
+
+    def state_callback(self, msg):
+
+
