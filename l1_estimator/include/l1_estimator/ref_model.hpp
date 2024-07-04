37323c75bea1bd72667cdcd5d93badc7cda04269
kay <kay1216@yonsei.ac.kr>
2024-07-04 19:19:48 +0900

    ref_model ongoing

---
+++ l1_estimator/include/l1_estimator/ref_model.hpp
@@ -0,0 +1,13 @@
+#ifndef REF_MODEL_HPP_
+#define REF_MODEL_HPP_
+#include "tools.hpp"
+#include <boost/lambda/lambda.hpp>
+#include <boost/numeric/odeint.hpp>
+#include <boost/numeric/odeint/external/eigen/eigen.hpp>
+
+using boost::numeric::odeint::runge_kutta4;
+
+
+
+
+#endif
