e585ee15adb60bb0897b6713d5febbfc603f0ec7
kay <kay1216@yonsei.ac.kr>
2024-07-05 20:41:56 +0900

    disturbance estimator default version

--- l1_estimator/include/l1_estimator/disturbance_estimator_default.hpp
+++ l1_estimator/include/l1_estimator/disturbance_estimator_default.hpp
@@ -18,12 +18,32 @@ class DisturbanceEstimator{
     const mat31_t& w_state, const quat_t& w_ref,
     const double& time);
 
+    void get_est_raw(mat31_t& sigma_est, mat31_t& theta_est) const;
 
-    void get_est_raw(mat31_t& sigma_est, mat31_t& theta_est);
+    void get_est_filtered(mat31_t& sigma_est, mat31_t& theta_est) const;
 
-    void get_est_filtered(mat31_t& sigma_est, mat31_t& theta_est);
+    void solve();
 
     private:
+
+    Inertial_param nominal_param_;
+
+    quat_t q_tilde_;
+    mat31_t p_tilde_, v_tilde_, w_tilde_;
+    mat31_t sigma_hat_, theta_hat_;
+    mat31_t dsigma_hat_, dtheta_hat_;
+    mat31_t sigma_hat_lpf_, theta_hat_lpf_;
+    state6_t s_;
+
+    double curr_time_, prev_time_, dt_;
+    
+    Convex_fn conv_fn_obj[2];
+    GammaPrj gamma_prj_obj[2];
+    runge_kutta4<state6_t> rk4;
+    
+    Lpf lpf_obj[2];
+
+    void system_dynamics(const state6_t& s, state6_t& dsdt, const double t);
 
 };
 
--- l1_estimator/include/l1_estimator/type_definitions.hpp
+++ l1_estimator/include/l1_estimator/type_definitions.hpp
@@ -17,7 +17,6 @@ typedef Matrix<double, 3, 3> mat33_t;
 typedef Matrix<double, 4, 4> mat44_t;
 typedef Matrix<double, 4, 1> mat41_t;
 
-typedef Matrix<double,6,1> mat61_t;
 typedef Matrix<double,6,4> mat64_t;
 
 typedef Matrix<double,6,1> state6_t;
