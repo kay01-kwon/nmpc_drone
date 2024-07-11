5e0bc4164d2367ed05feebc1d87699f70cc00bd7
kay <kay1216@yonsei.ac.kr>
2024-07-11 19:47:51 +0900

    test code on going

--- l1_estimator/src/test.cpp
+++ l1_estimator/src/test.cpp
@@ -1,9 +1,29 @@
+#include <ros/ros.h>
 #include "l1_estimator/l1_estimator.hpp"
 #include "yaml_converter/read_config.hpp"
 
 
 int main(int argc, char**argv)
 {
+    ros::init(argc, argv, "Test node");
+
+    ros::NodeHandle nh;
+
+    string simulation_param_dir,
+    nominal_param_dir;
+
+    nh.getParam("simulation_param", simulation_param_dir);
+    nh.getParam("nominal_param", nominal_param_dir);
+
+    ReadConfig* sim_param_read_ptr;
+    ReadConfig* nominal_param_read_ptr;
+
+    sim_param_read_ptr = 
+    new ReadConfig(simulation_param_dir);
+    
+    nominal_param_read_ptr = 
+    new ReadConfig(nominal_param_dir);
+
 
 
     return EXIT_SUCCESS;
