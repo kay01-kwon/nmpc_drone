5c4f01c0437c008e352d4815f66282545c769a58
kay <kay1216@yonsei.ac.kr>
2024-07-29 20:34:27 +0900

    commit

--- opt_based_estimator/include/tools/quaternion_math_tools.cpp
+++ opt_based_estimator/include/tools/quaternion_math_tools.cpp
@@ -1,5 +1,29 @@
 #include "quaternion_math_tools.h"
 
+quaternion_t otimes(const quaternion_t &q_left, const quaternion_t &q_right)
+{
+    mat44_t Lambda;
+    mat41_t qr, q_temp;
+    quaternion_t q_res;
+
+    Lambda << q_left.w(), -q_left.x(), -q_left.y(), -q_left.z(),
+            q_left.x(), q_left.w(), -q_left.z(), q_left.y(),
+            q_left.y(), q_left.z(), q_left.w(), -q_left.x(),
+            q_left.z(), -q_left.y(), q_left.x(), q_left.w();
+
+    qr << q_right.w(), q_right.x(), q_right.y(), q_right.z();
+
+
+    q_temp = Lambda*qr;
+
+    q_res.w() = q_temp(0);
+    q_res.x() = q_temp(1);
+    q_res.y() = q_temp(2);
+    q_res.z() = q_temp(3);
+
+    return q_res;
+}
+
 mat41_t otimes(const mat41_t &q_left, const mat41_t &q_right)
 {
     mat44_t Lambda;
