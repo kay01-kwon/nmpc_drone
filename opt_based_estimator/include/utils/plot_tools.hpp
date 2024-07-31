e26aa615c13b467ef86ff73a9197afbcb250df5f
kay <kay1216@yonsei.ac.kr>
2024-07-31 22:08:58 +0900

    set tick member function

--- opt_based_estimator/include/utils/plot_tools.hpp
+++ opt_based_estimator/include/utils/plot_tools.hpp
@@ -18,19 +18,37 @@ class PlotTool{
 
     public:
 
-    PlotTool() = default;
+    PlotTool();
 
-    void set_theta(const vector_t &theta, const vector_t &theta_est);
+    PlotTool(const size_t &size_of_data);
 
+    void set_keywords(const int &line_width, const int &label_font_size, const int &tick_font_size);
 
+    void add_data(const double &time, const vector_t &true_data, const vector_t &est_data);
+
+    void plot_data(const string &y_label, const string &data1_label, const string &data2_label);
 
     private:
-
-    vector<double> theta_x_est, theta_y_est, theta_z_est;
-    vector<double> theta_x_exg, theta_y_exg, theta_z_exg;
-
     
-    
+    void set_line_width(const int &line_width);
+
+    void set_font_size(const int &font_size);
+
+    void set_ticks(const int &x_tick_size, const int &y_tick_size);
+
+    map<string, string> line_keywords_;
+    map<string, string> label_keywords_;
+    map<string, string> tick_keywords_;
+    map<string, string> legend_keywords_;
+
+    vector<double> theta_x_est_, theta_y_est_, theta_z_est_;
+    vector<double> theta_x_exg_, theta_y_exg_, theta_z_exg_;
+
+    vector<double> x_tick_vec_;
+    vector<double> y_tick_vec_;
+
+    size_t data_size_;
+
 };
 
 
