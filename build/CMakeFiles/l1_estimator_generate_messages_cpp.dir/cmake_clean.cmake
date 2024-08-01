8e483a188f7fa18a7a368febe8b8149f8014e56b
kay <kay1216@yonsei.ac.kr>
2024-08-01 21:21:28 +0900

    simulation model bug fixed

--- build/CMakeFiles/l1_estimator_generate_messages_cpp.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/l1_estimator_generate_messages_cpp"
-  "devel/include/l1_estimator/Lpf_test.h"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/l1_estimator_generate_messages_cpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build/devel/share/l1_estimator/cmake/l1_estimator-msg-paths.cmake
+++
@@ -1,4 +0,0 @@
-# generated from genmsg/cmake/pkg-msg-paths.cmake.develspace.in
-
-set(l1_estimator_MSG_INCLUDE_DIRS "/home/kay/catkin_ws/src/research/l1_estimator/msg")
-set(l1_estimator_MSG_DEPENDENCIES std_msgs)
