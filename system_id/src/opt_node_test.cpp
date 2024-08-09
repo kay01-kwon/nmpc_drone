4b887b94b672bcb53fc23d72471981451a444424
kay <kay1216@yonsei.ac.kr>
2024-08-09 15:56:43 +0900

    delete the package named system_id

--- system_id/src/opt_node_test.cpp
+++
@@ -1,9 +0,0 @@
-#include "optimization_method/optimization_node.hpp"
-
-int main(int argc, char** argv)
-{
-    cout << "Test" << endl;
-    OptMethodName opt_method_name;
-    opt_method_name = OptMethodName::GradDesc;
-    OptNode<double> opt_node(opt_method_name);
-}
