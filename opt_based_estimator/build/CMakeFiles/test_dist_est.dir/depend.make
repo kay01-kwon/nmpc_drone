942f89fa251fc7cb9bf123b0d76918d2d0e6f94d
kay <kay1216@yonsei.ac.kr>
2025-03-27 04:02:53 +0900

    Dimension error fixed

--- opt_based_estimator/build/CMakeFiles/test_dist_est.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for test_dist_est.
-# This may be replaced when dependencies are built.
--- opt_based_estimator/build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/kay/catkin_ws/src/research/opt_based_estimator
-# Build directory: /home/kay/catkin_ws/src/research/opt_based_estimator/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
