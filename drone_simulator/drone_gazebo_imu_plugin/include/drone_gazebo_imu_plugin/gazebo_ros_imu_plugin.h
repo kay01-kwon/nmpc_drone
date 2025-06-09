9339d2b2cdb20bb9f1d7769612227c3ff40b1567
kay <kay1216@yonsei.ac.kr>
2025-06-09 19:59:17 +0900

    imu plugin added

---
+++ drone_simulator/drone_gazebo_imu_plugin/include/drone_gazebo_imu_plugin/gazebo_ros_imu_plugin.h
@@ -0,0 +1,127 @@
+#ifndef DRONE_GAZEBO_ROS_IMU_PLUGIN_H
+#define DRONE_GAZEBO_ROS_IMU_PLUGIN_H
+
+
+#include <random>
+#include <iostream>
+
+#include <Eigen/Core>
+
+#include <gazebo/common/common.hh>
+#include <gazebo/common/Plugin.hh>
+#include <gazebo/gazebo.hh>
+#include <gazebo/physics/physics.hh>
+#include <gazebo/physics/PhysicsTypes.hh>
+
+#include <ros/ros.h>
+#include <sensor_msgs/Imu.h>
+
+namespace gazebo{
+
+    // Default values for IMU parameters
+
+    // Pixhawk IMU default values
+
+    static constexpr double DefaultAccNoiseDensity = 0.0070708;
+
+    static constexpr double DefaultAccRandomWalk = 3.6921e-4;
+
+    static constexpr double DefaultGyroNoiseDensity = 0.0004326;
+
+    static constexpr double DefaultGyroRandomWalk = 1.2810e-5;
+
+    static constexpr double DefaultGravityMagnitude = 9.81;
+
+    struct ImuParameters{
+
+        // Accelerometer noise density [m/s/s/Hz]
+        double acc_noise_density;
+
+        // Accelerometer bias random walk [m/s/s/sqrt(Hz)]
+        double acc_random_walk;
+
+        // Gyroscope noise density  [rad/s/Hz]
+        double gyro_noise_density;
+
+        // Gyroscope bias random walk [rad/s/s/sqrt(Hz)]
+        double gyro_random_walk;
+
+        double gravitiy_magnitude;
+
+        // Default constructor initializing parameters to default values
+        ImuParameters()
+        : acc_noise_density(DefaultAccNoiseDensity),
+          acc_random_walk(DefaultAccRandomWalk),
+          gyro_noise_density(DefaultGyroNoiseDensity),
+          gyro_random_walk(DefaultGyroRandomWalk),
+          gravitiy_magnitude(DefaultGravityMagnitude)
+        {}
+    };
+
+    class GazeboRosImuPlugin : public ModelPlugin
+    {
+        public:
+
+        GazeboRosImuPlugin();
+        ~GazeboRosImuPlugin();
+
+        protected:
+            // Load the plugin
+            void Load(physics::ModelPtr _model, sdf::ElementPtr _sdf);
+
+            // Add noise to the IMU measurements
+            void AddNoise(
+                Eigen::Vector3d* linear_acceleration,
+                Eigen::Vector3d* angular_velocity,
+                double dt
+            );
+
+            // This callback is called by Gazebo 
+            // when the plugin is updated
+            void OnUpdate();
+
+        private:
+
+            std::string namespace_;
+            std::string imu_topic_;
+
+            std::string frame_id_;
+            std::string link_name_;
+
+            ros::NodeHandle *ros_node_handle_ptr_;
+            ros::Publisher ros_imu_publisher_;
+
+            // Pointer to world
+            physics::WorldPtr world_;
+            
+            // Pointer to the model
+            physics::ModelPtr model_;
+
+            // Pointer to the link
+            physics::LinkPtr link_;
+
+            // Connection Pointer to the update event
+            event::ConnectionPtr updateConnection_;
+
+            common::Time last_time_;
+
+            sensor_msgs::Imu ros_imu_msg_;
+
+            ignition::math::Vector3d gravity_W_;
+
+            Eigen::Vector3d acc_bias_;
+            Eigen::Vector3d gyro_bias_;
+
+            ImuParameters imu_params_;
+
+            std::random_device rd_{};
+            std::mt19937 gen_{rd_()};
+
+            std::normal_distribution<double> normal_dist_{0.0, 1.0};
+
+    };
+}
+
+
+
+#endif  //DRONE_GAZEBO_ROS_IMU_PLUGIN_H
---
+++ drone_simulator/drone_gazebo_imu_plugin/src/gazebo_ros_imu_plugin.cpp
@@ -0,0 +1,277 @@
+#include "drone_gazebo_imu_plugin/gazebo_ros_imu_plugin.h"
+
+#include <boost/bind.hpp>
+#include <chrono>
+
+
+namespace gazebo{
+
+    GZ_REGISTER_MODEL_PLUGIN(GazeboRosImuPlugin);
+
+    GazeboRosImuPlugin::GazeboRosImuPlugin()
+    : ModelPlugin(),
+    ros_node_handle_ptr_(nullptr),
+    acc_bias_(0, 0, 0),
+    gyro_bias_(0, 0, 0)
+    {}
+
+    GazeboRosImuPlugin::~GazeboRosImuPlugin()
+    {
+        ros_node_handle_ptr_->shutdown();
+        delete ros_node_handle_ptr_;
+    }
+
+    void GazeboRosImuPlugin::Load(physics::ModelPtr _model, sdf::ElementPtr _sdf)
+    {
+        _model->GetName();
+        model_ = _model;
+        world_ = model_->GetWorld();
+        gravity_W_ = world_->Gravity();
+        namespace_.clear();
+
+        if(_sdf->HasElement("robotNamespace"))
+        {
+            namespace_ = _sdf->Get<std::string>("robotNamespace");
+            std::cout << "[gazebo_imu_plugin]"
+            << "Robot namespace: " << namespace_ << "\n";
+        }
+        else
+        {
+            std::cout << "[gazebo_imu_plugin]"
+            << "Please specify a robot namespace. \n";
+        }
+
+        if(_sdf->HasElement("linkName"))
+        {
+            link_name_ = _sdf->Get<std::string>("linkName");
+            std::cout << "[gazebo_imu_plugin]"
+            << " Link name: " << link_name_ << "\n";
+        }
+        else
+        {
+            std::cout << "[gazebo_imu_plugin]"
+            << " Please specify a link name. \n";
+            return;
+        }
+
+        link_ = model_->GetLink(link_name_);
+        
+        if(link_ == nullptr)
+        {
+            std::cout << "[gazebo_imu_plugin]"
+            << " Link with name '" << link_name_ << "' not found. \n";
+            return;
+        }
+
+        std::cout << "[gazebo_imu_plugin] get sdf Param \n";
+
+        // 1. Initialize imu topic name
+        if(_sdf->HasElement("imuTopic"))
+        {
+            imu_topic_ = _sdf->Get<std::string>("imuTopic");
+            std::cout << "[gazebo_imu_plugin]"
+            << " IMU topic: " << imu_topic_ << "\n";
+        }
+        else
+        {
+            std::cout << "[gazebo_imu_plugin]"
+            << " Please specify an IMU topic name. \n";
+            return;
+        }
+
+        // 2. Initialize IMU parameters
+        // 2.1 Initialize the accelerometer noise density
+        if(_sdf->HasElement("accNoiseDensity"))
+        {
+            imu_params_.acc_noise_density = 
+            _sdf->Get<double>("accNoiseDensity");
+            std::cout << "[gazebo_imu_plugin]"
+            << " Accelerometer noise density: "
+            << imu_params_.acc_noise_density << "\n";
+        }
+        else
+        {
+            std::cout << "[gazebo_imu_plugin]"
+            << " Using default accelerometer noise density: "
+            << DefaultAccNoiseDensity << "\n";
+        }
+
+
+        // 2.2 Initialize the accelerometer random walk
+        if(_sdf->HasElement("accRandomWalk"))
+        {
+            imu_params_.acc_random_walk = 
+            _sdf->Get<double>("accRandomWalk");
+            std::cout << "[gazebo_imu_plugin]"
+            << " Accelerometer random walk: "
+            << imu_params_.acc_random_walk << "\n";
+        }
+        else
+        {
+            std::cout << "[gazebo_imu_plugin]"
+            << " Using default accelerometer random walk: "
+            << DefaultAccRandomWalk << "\n";
+        }
+
+        // 2.3 Initialize the gyro noise density
+        if(_sdf->HasElement("gyroNoiseDensity"))
+        {
+            imu_params_.gyro_noise_density = 
+            _sdf->Get<double>("gyroNoiseDensity");
+            std::cout << "[gazebo_imu_plugin]"
+            << " Gyroscope noise density: "
+            << imu_params_.gyro_noise_density << "\n";
+        }
+        else
+        {
+            std::cout << "[gazebo_imu_plugin]"
+            << " Using default gyroscope noise density: "
+            << DefaultGyroNoiseDensity << "\n";
+        }
+
+        // 2.4 Initialize the gyro random walk
+        if(_sdf->HasElement("gyroRandomWalk"))
+        {
+            imu_params_.gyro_random_walk = 
+            _sdf->Get<double>("gyroRandomWalk");
+            std::cout << "[gazebo_imu_plugin]"
+            << " Gyroscope random walk: "
+            << imu_params_.gyro_random_walk << "\n";
+        }
+        else
+        {
+            std::cout << "[gazebo_imu_plugin]"
+            << " Using default gyroscope random walk: "
+            << DefaultGyroRandomWalk << "\n";
+        }
+        
+        frame_id_ = link_name_;
+
+        std::cout << "[gazebo_imu_plugin] ROS node initialized \n";
+
+        // Initalize the transport node
+        ros_node_handle_ptr_ = new ros::NodeHandle(this->namespace_);
+        
+        std::cout << "[gazebo_imu_plugin] Allocate node on the heap\n";
+
+        ros_imu_publisher_ = ros_node_handle_ptr_->
+        advertise<sensor_msgs::Imu>
+        ("/" + namespace_ + "/" + imu_topic_, 1);
+
+        std::cout<< "[gazebo_imu_plugin] IMU publisher initialized \n";
+
+        ros_imu_msg_.header.frame_id = frame_id_;
+
+        // Connect the update event
+        std::cout << "[gazebo_imu_plugin] Connecting to update event \n";
+
+        this->updateConnection_ = 
+        event::Events::ConnectWorldUpdateBegin(
+            boost::bind(&GazeboRosImuPlugin::OnUpdate, this)
+        );
+
+        last_time_ = world_->SimTime();
+
+        ROS_INFO_STREAM(
+            "[gazebo_imu_plugin] IMU plugin loaded for model: "
+            << model_->GetName() << " on link: " << link_name_
+        );
+
+    }
+
+    void GazeboRosImuPlugin::OnUpdate()
+    {
+        common::Time current_time = world_->SimTime();
+        double dt = (current_time - last_time_).Double();
+
+        // Get the transformation matrix from world to the link
+        ignition::math::Pose3d tf_w_I = link_->WorldPose();
+        ignition::math::Quaterniond quat_w_I = tf_w_I.Rot();
+
+        // Get the linear acceleration of the imu link
+        ignition::math::Vector3d acc_I = 
+        link_->RelativeLinearAccel() - 
+        quat_w_I.RotateVectorReverse(gravity_W_);
+
+        // Get the angular velocity of the imu link
+        ignition::math::Vector3d ang_vec_I = 
+        link_->RelativeAngularVel();
+
+        Eigen::Vector3d linear_acc(
+            acc_I.X(),
+            acc_I.Y(),
+            acc_I.Z()
+        );
+
+        Eigen::Vector3d angular_vel(
+            ang_vec_I.X(),
+            ang_vec_I.Y(),
+            ang_vec_I.Z()
+        );
+
+        // Add noise and bias to the IMU measurement
+        AddNoise(&linear_acc, &angular_vel, dt);
+
+        ros_imu_msg_.header.stamp.sec = current_time.sec;
+        ros_imu_msg_.header.stamp.nsec = current_time.nsec;
+
+        ros_imu_msg_.orientation.w = quat_w_I.W();
+        ros_imu_msg_.orientation.x = quat_w_I.X();
+        ros_imu_msg_.orientation.y = quat_w_I.Y();
+        ros_imu_msg_.orientation.z = quat_w_I.Z();
+
+        ros_imu_msg_.linear_acceleration.x = linear_acc[0];
+        ros_imu_msg_.linear_acceleration.y = linear_acc[1];
+        ros_imu_msg_.linear_acceleration.z = linear_acc[2];
+
+        ros_imu_msg_.angular_velocity.x = angular_vel[0];
+        ros_imu_msg_.angular_velocity.y = angular_vel[1];
+        ros_imu_msg_.angular_velocity.z = angular_vel[2];
+
+        // Publish the ROS IMU message
+        ros_imu_publisher_.publish(ros_imu_msg_);
+        ros::spinOnce();
+
+        last_time_ = current_time;
+
+    }
+
+    void GazeboRosImuPlugin::AddNoise(
+    Eigen::Vector3d* linear_acc,
+    Eigen::Vector3d* angular_vel,
+    double dt)
+    {
+        GZ_ASSERT(linear_acc != nullptr,
+        "linear_acc cannot be null");
+        GZ_ASSERT(angular_vel != nullptr,
+        "angular_vel cannot be null");
+        GZ_ASSERT(dt > 0.0,
+        "dt must be greater than 0.0");
+
+        for(size_t i = 0; i < 3; i++)
+        {
+            // Add random walk to the bias
+            acc_bias_[i] += 
+            imu_params_.acc_random_walk 
+            * normal_dist_(gen_)
+            * dt;
+
+            gyro_bias_[i] +=
+            imu_params_.gyro_random_walk
+            * normal_dist_(gen_)
+            * dt;
+            
+            // Add noise and bias to the accelerometer and gyro
+            (*linear_acc)[i] = 
+            (*linear_acc)[i] + acc_bias_[i]
+            + imu_params_.acc_noise_density
+            * normal_dist_(gen_);
+            
+            (*angular_vel)[i] = 
+            (*angular_vel)[i] + gyro_bias_[i]
+            + imu_params_.gyro_noise_density
+            * normal_dist_(gen_);
+        }
+    }
+
+}
